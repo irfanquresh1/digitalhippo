0d5232eaad974bde1a82e96ece7969d636545ae1
Rahul Kumar <rahul@aerosimple.in>
2024-02-09 20:52:25 +0530

    image value update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo2-production.up.railway.app/",
-			},
-		],
+		domains: [
+			'localhost',
+			'digitalhippo2-production.up.railway.app'
+		]		
+		// remotePatterns: [
+		// 	{
+		// 		protocol: "http",
+		// 		hostname: "localhost",
+		// 	},
+		// 	{
+		// 		protocol: "https",
+		// 		hostname: "digitalhippo2-production.up.railway.app",
+		// 	},
+		// ],
 	},
 };
 
